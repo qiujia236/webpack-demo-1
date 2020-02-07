a03faaef4c2093e3aae9dd6eecc900f26eec1e20
wb <wbak123@sina.com>
2020-02-07 16:33:05 +0800

    第五次提交

--- src/index.js
+++ src/index.js
@@ -1,2 +1,23 @@
-import x from "./x.js";
-import css from "./x.css";
+import "./x.js";
+import "./x.scss";
+import "./y.less";
+import "./z.styl";
+import png from "./2020.png";
+
+const div = document.getElementById("app1");
+div.innerHTML = `<img src="${png}">`;
+
+const button = document.createElement("button");
+button.innerHTML = "懒加载";
+button.onclick = () => {
+  const promise = import("./lazy.js");
+  promise.then(
+    module => {
+      module.default();
+    },
+    () => {
+      console.error("模块加载错误");
+    }
+  );
+};
+div.appendChild(button);
--- webpack.config.base.js
+++ webpack.config.base.js
@@ -12,5 +12,37 @@ module.exports = {
       title: "wb",
       template: "src/test.html"
     })
-  ]
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"]
+      },
+
+      {
+        test: /\.styl$/,
+        loader: ["style-loader", "css-loader", "stylus-loader"]
+      },
+      {
+        test: /\.less$/,
+        loader: ["style-loader", "css-loader", "less-loader"] // compiles Less to CSS
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          {
+            loader: "sass-loader",
+            options: { implementation: require("dart-sass") }
+          }
+        ]
+      }
+    ]
+  }
 };
